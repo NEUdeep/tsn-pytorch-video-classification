813e2c864614df8625cf2f32f0e06b65cdd65c61
Kang Haidong <kanghaidong@supremind.com>
2019-05-31 15:31:00 +0800

    debug project and modified tran.py

--- script/.ipynb_checkpoints/V1.0_rgb_test-checkpoint.sh
+++ script/.ipynb_checkpoints/V1.0_rgb_test-checkpoint.sh
@@ -3,10 +3,9 @@
 #Kang Haidong
 #20190521
 #consensus_type == 'video'
-#train_list ==''
-#val_list == ''
+#test_list == ''
 #mixup data augmentation 
 #num_class==6
 #out of memory cuda module
 #pytorch1.0.1, cuda 9.0
-nohup python3 -u train.py --cfg ./configs/V1.0_train.yml > V1.0_2.1.5_out.out 2>&1 &
+nohup python3 -u train.py --cfg ./configs/V1.0_rgb_test.yml > V1.0_2.1.5_test_out.out 2>&1 &
